b18575099960b1d89197af4045d04ae0f0c4334e
lackadaisicalcynic <mikle.moiseev@mail.ru>
2019-11-17 00:21:12 +0300

    wow

--- src/main/scala/LogisticRegressionClassifier.scala
+++ src/main/scala/LogisticRegressionClassifier.scala
@@ -3,34 +3,53 @@ package src.main.scala
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import org.apache.spark.ml.feature.{HashingTF, Tokenizer}
-import org.apache.spark.ml.tuning.{CrossValidator, CrossValidatorModel, ParamGridBuilder}
+import org.apache.spark.ml.feature.{CountVectorizer, Tokenizer}
 import org.apache.spark.sql.DataFrame
 
 
-class LogisticRegressionClassifier(target: String, pred: String) {
+class LogisticRegressionClassifier(target: String, pred: String, data: DataFrame) {
 
   val labelColumn: String = target
   val predictionColumn: String = pred
+  val pathToModel: String = "trained_model"
 
-  // describing preprocessing steps
   private val tokenizer = new Tokenizer()
     .setInputCol("text")
     .setOutputCol("words")
 
-  private val hashingTF = new HashingTF()
-    .setInputCol(tokenizer.getOutputCol)
+//  private val hashingTF = new HashingTF()
+//    .setInputCol(tokenizer.getOutputCol)
+//    .setOutputCol("features")
+
+//  private val ngram = new NGram()
+//    .setN(2).setInputCol("filtered").
+//    setOutputCol("ngrams")
+
+//  val remover = new StopWordsRemover()
+//    .setInputCol("words")
+//    .setOutputCol("filtered")
+
+  private val cv: CountVectorizer = new CountVectorizer()
+    .setInputCol("words")
     .setOutputCol("features")
+    .setMinDF(10)
+
+
 
   private val lr = new LogisticRegression()
     .setLabelCol(labelColumn)
     .setFeaturesCol("features")
     .setPredictionCol(predictionColumn)
-    .setMaxIter(10)
+    .setElasticNetParam(0.8)
+    .setRegParam(0.001)
+    .setMaxIter(10000)
 
   private val stages = Array(
     tokenizer,
-    hashingTF,
+//    hashingTF,
+//    remover,
+//    ngram,
+    cv,
     lr
   )
 
@@ -38,32 +57,19 @@ class LogisticRegressionClassifier(target: String, pred: String) {
   private val evaluator = new MulticlassClassificationEvaluator()
     .setLabelCol(labelColumn)
     .setPredictionCol(predictionColumn)
-    .setMetricName("f1")
+    .setMetricName("accuracy")
 
   // creating a pipeline
   private val pipeline = new Pipeline().setStages(stages)
 
-  // get model with most suitable params
-  def crossValidate(df: DataFrame): CrossValidatorModel = {
+  val Array(trainingData, testData) = data.randomSplit(Array(0.8, 0.2))
+  val model = pipeline.fit(trainingData)
 
-    // cross validation
-    val paramGrid = new ParamGridBuilder()
-      .addGrid(hashingTF.numFeatures, Array(10, 100, 1000))
-      .addGrid(lr.regParam, Array(0.1, 0.01))
-      .build()
+  println(s"Test set accuracy = ${evaluator.evaluate(model
+    .transform(testData)
+    .select(labelColumn, predictionColumn))}")
 
-    val cv = new CrossValidator()
-      .setEstimator(pipeline)
-      .setEvaluator(evaluator)
-      .setEstimatorParamMaps(paramGrid)
-      .setNumFolds(3)
-      .setParallelism(2)
+  model.write.overwrite().save(pathToModel)
 
-    // fitting the model
-    val model: CrossValidatorModel = cv.fit(df)
-
-    model
-
-  }
 
 }
